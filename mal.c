a4bdee766fb3a6a76ca6678ff29628fdddc4694e
Ashni Mehta <ashnimehta@gmail.com>
2015-10-12 02:38:30 -0400

    finished up malloc
    we already have a free but bkr gave us another implementation of free later in class so we should decide which one we're going with

--- mal.c
+++ mal.c
@@ -22,4 +22,31 @@ void* myfree(void* p1){
 		root->isfree = 1;
 		initialized = 1;
 	}
+	p = root;
+	do{
+		if(p->size < size)
+			p = p->succ;
+		else if(!p->isfree)
+			p = p->succ;
+		else if(p->size < size + sizeof(mementry)) /*in BKR's notes he says he adds a fudge factor*/
+		{
+			p->isfree = 0;
+			return (char*) p + sizeof(mementry) + size;
+		}
+		else{
+			succ = (mementry*)((char*)p + sizeof(mementry)+size);
+			succ -> prev = p;
+			succ -> succ = p -> succ;
+			if(p->succ != 0)
+			{
+				p->succ->prev = succ;
+			}
+			p->succ = succ;
+			succ->size = p->size - sizeof(mementry) - size;
+			succ->isfree = 1;
+			p->size = size;
+			p->isfree = 0;
+			return ((char*) p + sizeof(mementry));
+		}
+	} /*should this be a do while because it's def not in the notes*/
 }
